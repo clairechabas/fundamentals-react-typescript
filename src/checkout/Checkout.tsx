13d039a6168ee88cf4c77409cce35ec01bef7f37
Claire Chabas <claire.chabas@gmail.com>
2023-01-24 13:30:06 +0000

    feat: use union types instead of enums

---
+++ src/checkout/Checkout.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+// type CheckoutStep = "Details" | "Shipping" | "Payment";
+enum CheckoutStep {
+  Details = "Details",
+  Shipping = "Shipping",
+  Payment = "Payment",
+}
+
+function Checkout() {
+  const [checkoutStep, setCheckoutStep] = useState<CheckoutStep>("Details");
+
+  return (
+    <>
+      {checkoutStep === "Details" && (
+        <>
+          <h1>Details</h1>
+          <button type="button" onClick={() => setCheckoutStep("Shipping")}>
+            Next
+          </button>
+        </>
+      )}
+
+      {checkoutStep === "Shipping" && (
+        <>
+          <h1>Shipping</h1>
+          <button type="button" onClick={() => setCheckoutStep("Payment")}>
+            Next
+          </button>
+        </>
+      )}
+      {checkoutStep === "Payment" && (
+        <>
+          <h1>Payment</h1>
+        </>
+      )}
+    </>
+  );
+}
+
+export default Checkout;
