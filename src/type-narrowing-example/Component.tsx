ca0ffcf36df0bd3ca7eb8175dbfa5a3709c6586a
Claire Chabas <claire.chabas@gmail.com>
2023-01-24 14:11:27 +0000

    feat: type narrowing use example

---
+++ src/type-narrowing-example/Component.tsx
@@ -0,0 +1,60 @@
+// Do
+interface ImageItem {
+  id: number;
+  title: string;
+  imageUrl: string;
+  // For equality check type narrowing:
+  type: "ImageItem";
+}
+interface QuoteItem {
+  id: number;
+  quote: string;
+  // For equality check type narrowing:
+  type: "QuoteItem";
+}
+export type Item = ImageItem | QuoteItem;
+
+// Don't:
+// export interface Item {
+//   id: string;
+//   title?: string;
+//   imageUrl?: string;
+//   quote?: string;
+// }
+
+interface ComponentProps {
+  items: Item[];
+}
+
+function Component({ items }: ComponentProps) {
+  return (
+    <ul>
+      {items.map((item) => {
+        // -- Option 1: type narrowing with the "in" operator
+        // if ("title" in item)
+        // -- Option 2: type narrowing with equality check on a `type` property
+        if (item.type === "ImageItem")
+          return (
+            <li key={item.id}>
+              {item.title && <p>{item.title}</p>}
+              {item.imageUrl && (
+                <img
+                  src={item.imageUrl}
+                  alt={item.title}
+                  style={{ maxWidth: "15rem" }}
+                />
+              )}
+            </li>
+          );
+
+        return (
+          <li key={item.id}>
+            {item.quote && <p style={{ fontStyle: "italic" }}>{item.quote}</p>}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Component;
---
+++ src/type-narrowing-example/Container.tsx
@@ -0,0 +1,21 @@
+import Component, { Item } from "./Component";
+
+function Container() {
+  const items: Item[] = [
+    {
+      id: crypto.randomUUID(),
+      title: "A nice picture",
+      imageUrl:
+        "https://images.unsplash.com/photo-1674560435460-c266654fdc56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+      id: crypto.randomUUID(),
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ea minima omnis blanditiis molestiae facilis nobis mollitia eos, consectetur magni quaerat quasi qui alias similique quisquam. Ipsum veritatis quo id.",
+    },
+  ];
+
+  return <Component items={items} />;
+}
+
+export default Container;
